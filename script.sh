18202997c125f725e963b1940636fd0a3d5bd274
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-02-02 21:50:25 -0500

    edit gitignore and script

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-python3 productList.py > html/productList.html
+python3 productListing.py > html/productListing.html
+python3 productDetail.py > html/productDetail.html
+python3 productPurchase.py > html/productPurchase.html
+python3 purchaseComplete.py > html/purchaseComplete.html
