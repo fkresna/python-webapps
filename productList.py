c9e6894d4963139d54a5a09ff9c3a252254a41b2
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-02-03 09:01:44 +0700

    product List html generator

---
+++ productList.py
@@ -0,0 +1,7 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(
+    loader=FileSystemLoader('themes/bootstrap4'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('productList.html')
+print(template.render(title='Product List'))
