ee53292f1d2a43de16ca04eef356e86cd5d7c810
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-02-05 10:24:10 +0700

    add product Listing

--- productListing.py
+++ productListing.py
@@ -1,7 +1,12 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import requests,json,config
+apiurl = config.API_URL
+r = requests.get(apiurl+'products')
+products = json.loads(r.text)
+
 env = Environment(
     loader=FileSystemLoader('themes/bootstrap4'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 template = env.get_template('productListing.html')
-print(template.render(title='Product List'))
+print(template.render(title='Product List',products=products,apiurl=apiurl))
