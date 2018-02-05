42f70f22489d2d314b9a93348ca0dd252f897b7e
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-02-05 10:25:06 +0700

    update productDetail

--- productDetail.py
+++ productDetail.py
@@ -1,7 +1,9 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import requests,json,config
+apiurl = config.API_URL
 env = Environment(
     loader=FileSystemLoader('themes/bootstrap4'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 template = env.get_template('productDetail.html')
-print(template.render(title='Product Detail'))
+print(template.render(title='Product Detail',apiurl=apiurl))
