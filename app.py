7c36c63d66f289ade218f48689a7b6d0c5081d5f
Freddy Kresna Tjoenedi <fkresna@gmail.com>
2018-02-05 10:25:33 +0700

    add productdetail

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Chalice(app_name='pythonwebapps')
 def index():
     return {'hello': 'world'}
 
+@app.route('/products')
+def productListing():
+	return [{'code':'1','name':'productName1','cost':30,'description':'product description','inventory_on_hand':10},{'code':'2','name':'productName2','cost':30,'description':'product description','inventory_on_hand':10},]
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/products/{code}')
+def productD1(code):
+	return productDetail(code)
+
+@app.route('/products/{code}/detail')
+def productD2(code):
+	return productDetail(code)
+
+@app.route('/products/{code}/purchase',methods=['POST'])
+def purchase(code):
+	return 'purchase'
+
+	
+
+def productDetail(code):
+	return {'code':code,'name':'productName','cost':30,'description':'product description','inventory_on_hand':10}
